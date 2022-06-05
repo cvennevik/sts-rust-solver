801673da77ad41b90429a18b00a824c42fd552c9
cvennevik <cvennevik@gmail.com>
2022-06-05 13:43:00 +0200

    feat(GameState): add GameState, Health, and Armor

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,41 @@
+#[derive(PartialEq, Debug)]
+pub struct Health(i32);
+#[derive(PartialEq, Debug)]
+pub struct Armor(i32);
+
+#[derive(PartialEq, Debug)]
+pub struct GameState {
+    player_health: Health,
+    player_armor: Armor,
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::{GameState, Health, Armor};
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn equal_gamestates_are_equal() {
+        let game_state_1 = GameState {
+            player_health: Health(1),
+            player_armor: Armor(0),
+        };
+        let game_state_2 = GameState {
+            player_health: Health(1),
+            player_armor: Armor(0),
+        };
+        assert_eq!(game_state_1, game_state_2);
+    }
+
+    #[test]
+    fn different_gamestates_are_not_equal() {
+        let game_state_1 = GameState {
+            player_health: Health(1),
+            player_armor: Armor(0),
+        };
+        let game_state_2 = GameState {
+            player_health: Health(1),
+            player_armor: Armor(1),
+        };
+        assert_ne!(game_state_1, game_state_2);
     }
 }
